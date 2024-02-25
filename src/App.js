c6c8e40746d504d7a3aed58ff542c0fe700e9415
B-Gerb <gerber.br@northeastern.edu>
2024-02-25 13:34:29 -0500

    supabase aadded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://zcpsievhvzshzdtfxzka.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 function App() {
   return (
     <div className="App">
