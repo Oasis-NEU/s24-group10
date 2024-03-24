a02b9c7f9ec26a9e78464ed30cec161a3f1b95d0
B-Gerb <gerber.br@northeastern.edu>
2024-03-24 13:08:04 -0400

    errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { createClient } from '@supabase/supabase-js'
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
+//import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zcpsievhvzshzdtfxzka.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+//const supabaseUrl = 'https://zcpsievhvzshzdtfxzka.supabase.co'
+//const supabaseKey = process.env.SUPABASE_KEY
+//const supabase = createClient(supabaseUrl, supabaseKey)
 function App() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);};
   return (
+    
+    
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -23,6 +37,46 @@ function App() {
         </a>
       </header>
     </div>
+    */
+    <html>
+      
+
+    <title>Family</title>
+
+    <div class="header">
+
+     <h1>LiveNEU</h1>
+     <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+
+      
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Home</MenuItem>
+        <MenuItem onClick={handleClose}>Walk Through</MenuItem>
+        <MenuItem onClick={handleClose}>Dorms</MenuItem>
+        <MenuItem onClick={handleClose}>About</MenuItem>
+
+      </Menu>
+     
+     </div>
+     
+    
+    </html>
   );
 }
 
