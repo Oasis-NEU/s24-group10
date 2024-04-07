f7a5a046547f7d7d7c5b07e6ebaea516e5ba7f3d
B-Gerb <gerber.br@northeastern.edu>
2024-04-07 02:06:47 -0400

    seperate web pages development in progress - small bug with housing portal not loading

--- src/types/Dorm.ts
+++ src/types/Dorm.ts
@@ -2,6 +2,16 @@
 export interface Dorm {
     id: number;
     name: string;
+    address: number;
+}
+
+export interface Address {
+    id: number;
+    street: string;
+    city: string;
+    state: string;
+    zipcode: number;
 }
 
 export const dormTable = "dorm_building";
+export const addressTable = "dorm_address";
